40068aa914dce45fb8585b9004f483b4c8d1f5ad
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-05-23 16:02:45 +0300

    add DOM manipulations to JS file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const title = document.querySelector('.title-input');
 const author = document.querySelector('.author-input');
 let books = JSON.parse('books', localStorage.getItem(books)) || [];
+const addbutton = document.querySelector('.add');
 
 function AddBooks(title, author){
     books.push({
@@ -10,8 +11,15 @@ function AddBooks(title, author){
     localStorage.setItem('books', JSON.stringify(books))
 }
 
-const createUI = ()=>{
+const createUI = ({title, author}) => {
     const newBookDiv = document.createElement('div');
+    newBookDiv.setAttribute('class', 'new-books');
+    const topBooks = document.querySelector('#top-books');
+    topBooks.append(newBookDiv);
+    newBookDiv.innerHTML = `<p>${title}</p>
+    <p>${author}</p>
+    <button type="submit">Remove</button>
+    <hr class="line">`
 }
 
-
+addbutton.addEventListener('submit', AddBooks())
