3164ea624c566cfc9346e8d11213462d1e94dff4
Olawale <olapetanwale@outlook.com>
2022-05-24 19:47:56 +0100

    Fix linter errors

--- Books.js
+++ Books.js
@@ -1,17 +1,17 @@
-export default class Books{
-    constructor(books){
-      this.books = books || [];
-    }
+export default class Books {
+  constructor(books) {
+    this.books = books || [];
+  }
 
-    addBooks(title, author){
-        this.books.push({
-            title,
-            author,
-        });
-    }
+  addBooks(title, author) {
+    this.books.push({
+      title,
+      author,
+    });
+  }
 
-    removeBooks(author){
-         this.author = author;
-         this.books = this.books.filter((book)=> book.author !== author)
-    }
+  removeBooks(author) {
+    this.author = author;
+    this.books = this.books.filter((book) => book.author !== author);
+  }
 }
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import Books from "./Books.js";
+import Books from './Books.js';
 
 const title = document.querySelector('.title-input');
 const author = document.querySelector('.author-input');
 const awesomeBooks = document.getElementById('awesome-books');
 const newBookDiv = document.querySelector('.new-books');
 
-let library = new Books(JSON.parse(localStorage.getItem('books'))) || [];
+const library = new Books(JSON.parse(localStorage.getItem('books'))) || [];
 
 const createUI = ({ title, author }) => {
   const bookContainer = document.createElement('div');
-  bookContainer.setAttribute('class', 'collection')  
+  bookContainer.setAttribute('class', 'collection');
   bookContainer.innerHTML = `<p>${title} by ${author}</p>
   <p class="hidden">${author}</p>
     <button id="remove-btn" type="submit">Remove</button>`;
@@ -19,21 +19,21 @@ const createUI = ({ title, author }) => {
 library.books.forEach((element) => createUI(element));
 
 awesomeBooks.onsubmit = (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
   newBookDiv.innerHTML = '';
   library.addBooks(title.value, author.value);
-  library.books.forEach((item)=>createUI(item))
+  library.books.forEach((item) => createUI(item));
   localStorage.setItem('books', JSON.stringify(library.books));
   title.value = '';
   author.value = '';
 };
-  
-  document.body.addEventListener('click', (event) => {
-      if (event.target.id.includes('remove-btn')) {
-          const parentDiv = event.target.parentNode;
-          const removebookAuthor = event.target.previousElementSibling.textContent;
-          library.removeBooks(removebookAuthor)
-          newBookDiv.removeChild(parentDiv);
-          localStorage.setItem('books', JSON.stringify(library.books));
-      }
-  });
+
+document.body.addEventListener('click', (event) => {
+  if (event.target.id.includes('remove-btn')) {
+    const parentDiv = event.target.parentNode;
+    const removebookAuthor = event.target.previousElementSibling.textContent;
+    library.removeBooks(removebookAuthor);
+    newBookDiv.removeChild(parentDiv);
+    localStorage.setItem('books', JSON.stringify(library.books));
+  }
+});
