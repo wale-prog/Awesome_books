8b28930c8ca29fa18f2c73a69705ffb3845406f8
Olawale <olapetanwale@outlook.com>
2022-05-25 15:28:14 +0100

    Add eventlisters to list and addnew

--- index.js
+++ index.js
@@ -41,13 +41,26 @@ document.body.addEventListener('click', (event) => {
   }
 });
 
- const contactbtn = document.querySelector('.contact-btn');
- const topbooks = document.querySelector('#top-books');
- const contact = document.querySelector('.background');
- const bookcollection = document.querySelector('.collection'); 
+ const contact = document.querySelector('.contact');
+ const list = document.getElementById('top-books');
+ const addNew = document.querySelector('.add-new');
+ const navList = document.querySelectorAll('.nav-list-item');
 
- contactbtn.addEventListener('click', () => {
-  contact.classList.remove('hidden');
-  topbooks.classList.add('hideen');
-   bookcollection.classList.add('hidden'); 
- });
+ for (let i = 0; i < navList.length; i+=1){
+   navList[i].addEventListener('click', (event)=>{
+     if (event.target.innerText === 'List'){
+       list.classList.remove('hidden'); 
+       contact.classList.add('hidden');
+       addNew.classList.add('hidden');
+     }else if (event.target.innerText === 'Add new'){
+        list.classList.add('hidden');
+        contact.classList.add('hidden');
+        addNew.classList.remove('hidden');
+     }else if(event.target.innerText === 'Contact'){
+        list.classList.add('hidden');
+        contact.classList.remove('hidden');
+        addNew.classList.add('hidden');
+     }
+   })
+ }
+ 
